77cd4f367d04e634a4abddcda48be73823ab217d
jadalrehaoui <jrehaoui.dev@outlook.com>
2026-06-13 11:55:19 -0600

    chore: release android-v5.2.3 windows-v5.5.2

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,7 +1,7 @@
 import 'dart:io' show Platform;
 
-const String appVersionAndroid = '5.2.2';
-const String appVersionWindows = '5.5.1';
+const String appVersionAndroid = '5.2.3';
+const String appVersionWindows = '5.5.2';
 
 String get appVersion =>
     Platform.isAndroid ? appVersionAndroid : appVersionWindows;
